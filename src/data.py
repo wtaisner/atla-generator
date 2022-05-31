db19a2b41e18af8e2a12dbee24f8e9676ebcc988
jkarolczak <jacek.karolczak@outlook.com>
2022-05-31 19:57:14 +0200

    Fix mypy

--- src/data.py
+++ src/data.py
@@ -6,8 +6,8 @@ def read_dataframe(path: str = '../data/avatar.csv') -> pd.DataFrame:
     df = df[df['character'] != 'Scene Description']
     df = df[~df['character'].str.contains('and')]
     df['character'] = df['character'].str.lower()
-    df['character'] = df['character'].str.replace(':|actor|actress', '', regex=True)
-    df['character'] = df['character'].str.title()
+    df['character'] = df['character'].str.replace(':|actor|actress', '', regex=True)  # type: ignore
+    df['character'] = df['character'].str.title()  # type: ignore
     df = df.reset_index()
     df = df.drop(columns=['index'])
     return df
